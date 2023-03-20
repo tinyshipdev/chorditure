8f5c69be08e200f1b3910b32e751dd74556e0cd9
Adam Gray <adamgrayscale@gmail.com>
2023-03-20 10:40:34 -0500

    make editor somewhat useable on mobile

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -23,6 +23,7 @@ interface Props {
 }
 
 const Editor: React.FC<Props> = ({}) => {
+  const [isEditorVisible, setIsEditorVisible] = useState(true);
   const [rawLyrics, setRawLyrics] = useState('');
 
   useEffect(() => {
@@ -42,11 +43,19 @@ const Editor: React.FC<Props> = ({}) => {
   }, [rawLyrics])
 
   return (
-    <div className='flex'>
+    <div className='md:flex'>
 
-      <textarea className='w-[30%] min-h-screen p-10 bg-slate-50 focus:outline-none' value={rawLyrics} onChange={(e) => setRawLyrics(e.target.value)} name="lyrics" id="lyrics"></textarea>
+      <button className='bg-slate-500	text-white py-0.5 px-2 rounded md:hidden absolute top-4 right-4' onClick={() => setIsEditorVisible(!isEditorVisible)}>
+        {isEditorVisible ? 'preview' : 'edit'}
+      </button>
 
-      <div className='w-[70%] min-h-screen p-10 wrapper'>
+      <textarea 
+        className={`w-full md:w-[30%] min-h-screen p-10 bg-slate-50 focus:outline-none ${isEditorVisible ? 'block' : 'hidden'} md:block`} 
+      value={rawLyrics} onChange={(e) => setRawLyrics(e.target.value)} name="lyrics" id="lyrics"></textarea>
+
+      <div 
+        className={`w-full md:w-[70%] min-h-screen p-10 wrapper ${!isEditorVisible ? 'block' : 'hidden'} md:block`}
+      >
         <Sheet song={parseSong(rawLyrics)}/>
       </div>
     </div>
