ab521b51a85144a71a783b2c3763ba384e852321
Adam Gray <adamgrayscale@gmail.com>
2023-03-24 10:45:59 +0000

    update icons to be maskable, hopefully improved cached urls

--- public/sw.js
+++ public/sw.js
@@ -1,26 +1,44 @@
-self.addEventListener("install", function (event) {
-  console.log("Hello world from the Service Worker 🤙");
+const CACHE_NAME = 'v1';
+
+const CACHE_URLS = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/app.js",
+  "/logo.svg",
+  "/paramore/crushcrushcrush"
+]
+
+const addResourcesToCache = async (resources) => {
+  const cache = await caches.open(CACHE_NAME);
+
+  try {
+    await cache.addAll(resources);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+self.addEventListener("install", async function (event) {
+  console.log("Service Worker installed");
 
   event.waitUntil(
-    addResourcesToCache([
-      "/",
-      "/index.html",
-      "/style.css",
-      "/app.js",
-      "/image-list.js",
-      "/star-wars-logo.jpg",
-      "/gallery/bountyHunters.jpg",
-      "/gallery/myLittleVader.jpg",
-      "/gallery/snowTroopers.jpg",
-    ])
+    caches.open("v1")
+      .then((cache) => cache.addAll([
+        "/", 
+        '/logo.svg', 
+        '/paramore/crushcrushcrush',
+        '/macy-gray/i-try'
+      ]))
   );
 });
 
 self.addEventListener('fetch', function(event) {
+  console.log(event);
   event.respondWith(async function() {
      try{
-       var res = await fetch(event.request);
-       var cache = await caches.open('cache');
+       const res = await fetch(event.request);
+       const cache = await caches.open(CACHE_NAME);
        cache.put(event.request.url, res.clone());
        return res;
      }
