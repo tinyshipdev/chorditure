7d652afd7f6a206aa9dd720c171fb57057be8900
Adam Gray <adamgrayscale@gmail.com>
2023-03-27 13:05:02 +0100

    enable viewing source correctly with transposition

--- src/components/Sheet.tsx
+++ src/components/Sheet.tsx
@@ -10,26 +10,46 @@ const Sheet: React.FC<Props> = ({ data }) => {
   const router = useRouter();
   const [transpose, setTranspose] = useState(0);
 
-  const song = parseSong(data, transpose, router?.query?.source ? true : false);
+  const isSourceView = router?.query?.source ? true : false;
+
+  const song = parseSong(data, transpose, isSourceView);
+
+  const transposeButton = (
+    <div className="flex mb-10">
+      <div>
+        <button className='mr-4 p-2' onClick={() => setTranspose(transpose - 1 <= -12 ? 0 : transpose - 1)}>-</button>
+        <span>transpose({transpose})</span>
+        <button className='ml-4 p-2' onClick={() => setTranspose(transpose + 1 >= 12 ? 0 : transpose + 1)}>+</button>
+      </div>
+    </div>
+  )
 
   return (
     <div>
 
-      <div className='py-10'>
-        <small>{song?.artist}</small>
-        <h1 className='font-bold text-4xl'>{song?.title}</h1>
-      </div>
-
-      <div className="flex mb-10">
+      {isSourceView ? (
         <div>
-          <button className='mr-4 p-2' onClick={() => setTranspose(transpose - 1 <= -12 ? 0 : transpose - 1)}>-</button>
-          <span>transpose({transpose})</span>
-          <button className='ml-4 p-2' onClick={() => setTranspose(transpose + 1 >= 12 ? 0 : transpose + 1)}>+</button>
+          <div>{transposeButton}</div>
+          <div>[title]</div>
+          <div>{song.title}</div>
+          <br />
+          <div>[artist]</div>
+          <div>{song.artist}</div>
+          <br />
+          <div>[lyrics]</div>
         </div>
-      </div>
+      ) : (
+        <div>
+          <div className='py-10'>
+            <small>{song?.artist}</small>
+            <h1 className='font-bold text-4xl'>{song?.title}</h1>
+          </div>
+          <div>{transposeButton}</div>
+        </div>
+      )}
 
-      <div 
-        className='whitespace-pre-wrap' 
+      <div
+        className='whitespace-pre-wrap'
         dangerouslySetInnerHTML={{ __html: song.lyrics }}
       ></div>
     </div>
